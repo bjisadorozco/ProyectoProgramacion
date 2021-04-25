158c301b254c78deecc5e0a92a9c0821b1380c26
ychimas3@outlook.com <ychimas3@outlook.com>
2021-04-25 13:05:23 -0500

    factura y servicio

---
+++ Entity/Servicios.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Servicios
+    {
+        
+            public String Nombre { get; set; }
+            public Double Precio { get; set; }
+            public Boolean Acompañante { get; set; }
+            public Double Descuento { get; set; }
+            public String Codigo { get; set; }
+        
+    }
+}
